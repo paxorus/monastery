3aa7c49070261754dc4b1da41312ef8bdcd17c2c
paxorus <prakhar@brandeis.edu>
2016-02-25 19:12:24 -0500

    ExpressJS, MongoLab test
    Ideally this will deploy to Heroku and display the document in my new
    collection in my tectonic database on MongoLab.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
+var MongoClient = require('mongodb').MongoClient;
+var format = require('util').format;
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -11,15 +13,27 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+	response.render('pages/index');
 });
 
 app.get('/cool', function(request, response) {
-  response.send(cool());
+	response.send(cool());
+});
+
+app.get('/db', function(req,res){
+	mongo(req,res);
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+	console.log('Node app is running on port', app.get('port'));
 });
 
-
+function mongo(req,res){
+	// return all Pokemon names
+	MongoClient.connect('mongodb://tectonic:2birds2stones@ds017258.mlab.com/seismic-test', function(err, db) {
+		if(err) throw err;
+		var collection = db.collection('new');
+		var query=collection.find();
+		res.render('pages/db',query);
+	});
+}

ef562022313bbeaf1406fd2bc188476b032e1d57
paxorus <prakhar@brandeis.edu>
2016-02-27 04:27:41 -0500

    MongoHelper, and insertion with confirmation
    MongoHelper class maps HTTP method to MongoDB function, <ul> of records
    generated via GET, add Pokemon via PUT, text color green->blue upon
    confirmation.

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-// var cool = require('cool-ascii-faces');
+// Prakhar Sahay 02/26/2016
+// node index.js
+
 var express = require('express');
 var app = express();
 var MongoClient = require('mongodb').MongoClient;
 var format = require('util').format;
+var querystring=require('querystring');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -16,73 +19,80 @@ app.get('/', function(request, response) {
 	response.render('pages/index');
 });
 
-app.get('/cool', function(request, response) {
-	response.send(cool());
+app.get('/db', function(req,res){
+	new MongoHelper({
+		db:'seismic-test',
+		collection:'new',
+		operation:'find',
+		then:function(docs){res.render('pages/db',{docs:docs})}
+	});
 });
 
-app.get('/db', function(req,res){
-	mongo(req,res);
-	// res.render('pages/db',{name:"Paxorus"});
+app.put('/db', function(req,res){
+	// read in PUT body
+	var body=[];
+	req.on("data",function(chunk){
+		body.push(chunk);
+	}).on("end",function(){
+		body=Buffer.concat(body).toString();
+		var data=querystring.parse(body);
+		new MongoHelper({
+			db:'seismic-test',
+			collection:'new',
+			operation:'insert',
+			data:data,
+			then:function(success){res.send(success)}
+		});
+	});
 });
 
 app.listen(app.get('port'), function() {
 	console.log('Node app is running on port', app.get('port'));
 });
 
-function mongo(req,res){
-	// return all Pokemon names
-	// var mongoConn=new MongoConn('seismic-test','new');
-	// mongoConn.find(function(docs){
-	// 	res.render('pages/db',{name:""+docs.length});
-	// });
-	var mongoConn=new MongoConn('seismic-test','new',function(docs){
-		res.render('pages/db',docs[0]);
-	});
-}
 
-function MongoConn(dbName,collName,render){
-	// this.queue=new Queue();
-	var x=this;
-	MongoClient.connect('mongodb://tectonic:2birds2stones@ds017258.mlab.com:17258/'+dbName, function(err, db) {
-		if(err) throw err;
-		x.db=db;
-		x.collection=db.collection(collName);
-		x.findBack(render);
-		// this.queue.next();
-	});
 
-	// this.find=function(render){
-	// 	this.queue.wait(function(render){findBack(render)});
-	// }
 
-	this.findBack=function(render){
+
+
+function MongoHelper(params){
+	var helper=this;
+	init("tectonic","2birds2stones");
+
+	function init(username,password){
+		var url=["mongodb://",username,":",password,"@ds017258.mlab.com:17258/",params.db].join("");
+		MongoClient.connect(url,function(err,db){
+			if(err) throw err;
+			helper.db=db;// get db
+			helper.collection=db.collection(params.collection);// get collection
+
+			switch(params.operation){// call correct operation
+				case "insert":helper.insert();break;
+				case "find":helper.find();break;
+				case "post":break;
+				case "remove":break;
+			}
+		});
+	}
+	this.find=function(){
 		var docs=[];
-		var cursor=x.collection.find();
+		var cursor=helper.collection.find();
 		cursor.each(function(err,doc){
 			if(err) throw err;
 			if(doc!=null){
 				docs.push(doc);
 			}else{
-				x.db.close();
-				render(docs);
+				helper.db.close();
+				params.then(docs);
 			}
 		});
-		// this.queue.next();
+	}
+	this.insert=function(){
+		this.collection.insertOne({name:params.data.name},function(err,result){
+			if(err) throw err;
+			helper.db.close();
+			params.then(result);
+		});
 	}
 
-
 }
-
-// function Queue(){
-// 	this.array=[];
-
-// 	this.wait=function(callback){
-// 		this.array.push(callback);
-// 	}
-
-// 	this.next=function(){
-// 		if(array.length==0) return;
-// 		var func=unshift(this.array);
-// 		func();
-// 	}
-// }
